5e7945fd9d0916f1f3cfc4ee147955f5f30b5cea
YoungCheonChoi <justinyoungcheon@gmail.com>
2021-03-15 10:18:18 +0900

    에이콘 안드로이드+코틀린

---
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+//app 을 실행 했을때 처음 사용자를 대면하는 Activity
+public class MainActivity extends AppCompatActivity {
+
+}
